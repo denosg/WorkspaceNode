fc670a564ae283adfa2682c9ec20fbae6f146915
denos <costelasdenis@gmail.com>
2023-10-25 17:59:15 +0300

    handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,39 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Costelas',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Despre mn',
+        name: 'Costelas',
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpMsg: 'Ajutor <3',
+        name: 'Costelas',
+    })
+})
+
 
 app.get('/weather', (req, res) => {
     res.send({
