5f019ef9cbf42f0251c295ae494be28ec26add7c
denos <costelasdenis@gmail.com>
2023-11-04 17:47:24 +0200

    styel + getting string query from https response req

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -41,9 +41,29 @@ app.get('/help', (req, res) => {
 
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an addres term',
+        })
+    }
+
     res.send({
         forecast: 'cacat',
-        location: 'zalau'
+        location: 'zalau',
+        address: req.query.address,
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term',
+        })
+    }
+
+    console.log(req.query)
+    res.send({
+        products: [],
     })
 })
 
