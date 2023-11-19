29861b95bd013ec6752038e3e6ac52ae65396e8f
denos <costelasdenis@gmail.com>
2023-11-19 15:58:53 +0200

    UD methods

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -17,21 +17,34 @@ async function main() {
     console.log('Connected successfully to server')
     const db = client.db(dbName)
 
-    const user = await db.collection('users').findOne({ _id: new ObjectId("655a046a7d34634f13cddcde") })
-    if (user) {
-        console.log(user.name)
-    }
-
-    const users = await db.collection('users').find({ age: { $gte: 21 } }).toArray();
-    if (users) {
-        console.log(users);
-    } else {
-        console.log('No users found with age greater than 21');
-    }
+    await db.collection('users').updateOne({ _id: new ObjectId("6559ffe662da3532a0a55834") }, {
+        // $set: {
+        //     name: "Andrei"
+        // }
+        $inc: {
+            age: 1
+        }
+    }).then((res) => {
+        console.log(`Updated succesfully. Res: ${res.modifiedCount}`)
+    }).catch((err) => {
+        console.log(`Err: ${err}`)
+    })
 
 }
 
 main().finally(() => client.close());
+
+// const user = await db.collection('users').findOne({ _id: new ObjectId("655a046a7d34634f13cddcde") })
+// if (user) {
+//     console.log(user.name)
+// }
+
+// const users = await db.collection('users').find({ age: { $gte: 21 } }).toArray();
+// if (users) {
+//     console.log(users);
+// } else {
+//     console.log('No users found with age greater than 21');
+// }
 
 // await db.collection('users').insertOne({
 //     _id: id,
