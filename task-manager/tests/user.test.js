f780641e879b199d6459aa7fa109920ae786449f
denos <costelasdenis@gmail.com>
2023-12-16 15:29:55 +0200

    more tests patch

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -63,3 +63,36 @@ test('should not get user profile unauthenticated user', async () => {
         .send()
         .expect(401)
 })
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/pozaCuMnMica.jpg')
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Andrei'
+        })
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.name).toBe('Andrei')
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'Zalau'
+        })
+        .expect(400)
+})
+
